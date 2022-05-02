ed395b815dddf6e1ea0693610c8f5d68adc04748
anchem <hackerleon@foxmail.com>
2022-05-02 19:02:25 +0800

    add thoughts

--- src/pages/thoughts.js
+++ src/pages/thoughts.js
@@ -46,22 +46,6 @@ function MyThoughtsScript() {
 
 export default function MyThoughts() {
   const {siteConfig} = useDocusaurusContext();
-  var gitalk = new Gitalk({
-    clientID: '8f8b00c0f9a02da92827',
-    clientSecret: 'e9e5f5370d1497b34960a41d0027faf1ee508cf1',
-    repo: 'anchem.github.io',
-    owner: 'anchem',
-    admin: ['anchem'],
-    id: location.pathname,      // Ensure uniqueness and length less than 50
-    title: 'thoughts_repo',
-    distractionFreeMode: false,  // Facebook-like distraction free mode
-    createIssueManually: true,
-    labels: ['comment','Gitalk'],
-    language: 'zh-CN',
-    pagerDirection: 'last',
-    isLocked: true
-  })
-  gitalk.render('gitalk-container')
   return (
     <Layout
       title={`随想`}>
