026a5704dd24e3bf87711b0aef25be645d332c10
hackerleon <la459715360@qq.com>
2022-08-09 22:36:32 +0800

    Update thoughts.js

--- src/pages/thoughts.js
+++ src/pages/thoughts.js
@@ -6,22 +6,7 @@ import Gitalk from 'gitalk'
 import GitalkComponent from "gitalk/dist/gitalk-component";
 
 export default function Thoughts() {
-  return (
-    <Layout title="Thoughts" description="my thoughts">
-      <BrowserOnly fallback={<div></div>}>{() => <GitalkComponent options={options} />}</BrowserOnly>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '50vh',
-          fontSize: '20px',
-        }}>
-        <p>
-          Thoughts
-        </p>
-      </div>
-      <GitalkComponent options={{
+  const {options} = <GitalkComponent options={{
         clientID: '8f8b00c0f9a02da92827',
         clientSecret: 'a0cbb95598b1c9132a2f82b2389ce1b93bdd20b1',
         repo: 'anchem.github.io',
@@ -35,7 +20,24 @@ export default function Thoughts() {
         language: 'zh-CN',
         pagerDirection: 'last',
         isLocked: true
-      }} />
+      }} />;
+  return (
+    <Layout title="Thoughts" description="my thoughts">
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '50vh',
+          fontSize: '20px',
+        }}>
+        <p>
+          Thoughts
+        </p>
+      </div>
+      <BrowserOnly fallback={<div>Loading...</div>}>
+        {() => <GitalkComponent options={options} />}
+      </BrowserOnly>
     </Layout>
   );
 }
