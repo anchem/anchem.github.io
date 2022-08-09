26ecf927b246939d8d931e22c92eced779d0d75a
hackerleon <la459715360@qq.com>
2022-08-09 21:58:19 +0800

    Create thoughts.js

---
+++ src/pages/thoughts.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+import 'gitalk/dist/gitalk.css'
+import Gitalk from 'gitalk'
+import GitalkComponent from "gitalk/dist/gitalk-component";
+
+export default function Thoughts() {
+  return (
+    <Layout title="Thoughts" description="my thoughts">
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '50vh',
+          fontSize: '20px',
+        }}>
+        <p>
+          Thoughts
+        </p>
+      </div>
+      <GitalkComponent options={{
+        clientID: '8f8b00c0f9a02da92827',
+        clientSecret: 'a0cbb95598b1c9132a2f82b2389ce1b93bdd20b1',
+        repo: 'anchem.github.io',
+        owner: 'anchem',
+        admin: ['anchem'],
+        labels: ['thoughts']
+        id: thoughts,      // Ensure uniqueness and length less than 50
+        distractionFreeMode: false  // Facebook-like distraction free mode
+      }} />
+    </Layout>
+  );
+}
