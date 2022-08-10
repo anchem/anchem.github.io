97ceda98bed1fb22402f0cb5380c5b921ecbf31e
hackerleon <la459715360@qq.com>
2022-08-10 12:09:13 +0800

    Update thoughts.js

--- src/pages/thoughts.js
+++ src/pages/thoughts.js
@@ -1,38 +1,7 @@
 import React from 'react';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
-
-const SentenceList = [
-    {
-        time: '2022-08-10',
-        content: (<>测试数据 1</>)
-    },
-    {
-        time: '2022-08-09',
-        content: (<>测试数据 2 测试数据 2 测试数据 2 测试数据 2 测试数据 2 测试数据 2 测试数据 2 测试数据 2 测试数据 2 测试数据 2 测试数据 2 测试数据 2 测试数据 2 测试数据 2</>)
-    }
-]
-
-function Sentence({time, content}) {
-  return (
-    <div>
-      <h4>{time}</h4>
-      <p>{content}</p>
-    </div>
-  );
-}
-
-function SentenceComp() {
-  return (
-  <div className="container">
-    <div className={clsx('col col--8')}>
-      {SentenceList.map((props, idx) => (
-        <Sentence key={idx} {...props} />
-      ))}
-    </div>
-  </div>
-  );
-}
+import SentenceComp from '@site/src/components/Thoughts';
 
 export default function Thoughts() {
   return (
